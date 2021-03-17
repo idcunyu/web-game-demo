6a11ba003a0ff3da5cab565b62a7f8a40baad7de
idcunyu <695035427@qq.com>
2021-03-17 22:38:36 +0800

    rm build

--- docs/components/index.js
+++
@@ -1,9 +0,0 @@
-export { default as GameBox } from '../../components/game/box.js'
-export { default as GameDrop } from '../../components/game/drop.js'
-export { default as GameContainer } from '../../components/game/game-container.vue'
-export { default as GamePlay } from '../../components/game/game-play.vue'
-
-export const LazyGameBox = import('../../components/game/box.js' /* webpackChunkName: "components/game-box" */).then(c => c.default || c)
-export const LazyGameDrop = import('../../components/game/drop.js' /* webpackChunkName: "components/game-drop" */).then(c => c.default || c)
-export const LazyGameContainer = import('../../components/game/game-container.vue' /* webpackChunkName: "components/game-container" */).then(c => c.default || c)
-export const LazyGamePlay = import('../../components/game/game-play.vue' /* webpackChunkName: "components/game-play" */).then(c => c.default || c)
